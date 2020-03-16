5fdf3f2f2a6ea824c569cfc23943d3bce14e54f7
nqazimn <nqazimn@gmail.com>
2020-03-15 23:39:12 -0500

    cleaned up some comments

--- mission_to_mars/app.py
+++ mission_to_mars/app.py
@@ -4,27 +4,18 @@ import scrape_mars
 
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/mission_to_mars_app"
-# mongo = PyMongo(app)
-
-# Or set inline
+# Set-up connection to database
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mission_to_mars_app")
 
 
 @app.route("/")
 def index():
-    # return "Welcome"
     listings = mongo.db.mission_to_mars_app.find_one()
-    # print(listings)
-    # team_list = ["test1", "test2", "test3"]
     return render_template("index.html", listings=listings)
 
 
 @app.route("/scrape")
 def scraper():
-    # print("Testing...")
-    # return "Welcome to 'Mission to Mars' page!"
     listings = mongo.db.mission_to_mars_app
     mars_data = scrape_mars.scrape()
     listings.update({}, mars_data, upsert=True)
